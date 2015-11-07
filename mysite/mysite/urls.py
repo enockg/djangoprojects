33a9110a012e87bb929cecbb6e1183aa94a78f23
enockg <enockglidden@hotmail.com>
2015-11-06 21:08:38 -0500

    django projects

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from mysite0.views import hello, current_datetime
 
 
-urlpatterns = [
+urlpatterns = patterns('',
    #url(r'^admin/', include(admin.site.urls)),
 	url(r'^hello/$', hello),
     url(r'^time/$', current_datetime),
@@ -26,4 +26,4 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
 	
-]
+)
