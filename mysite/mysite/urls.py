418b431a51bc999114c29eec3d8d21dc2444abc7
enockg <enockglidden@hotmail.com>
2015-11-06 21:05:34 -0500

    django projects

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,12 +13,17 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from mysite.views import hello
+from mysite0.views import hello, current_datetime
 
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+   #url(r'^admin/', include(admin.site.urls)),
 	url(r'^hello/$', hello),
+    url(r'^time/$', current_datetime),
+    url(r'^$', hello),
+
+    url(r'^admin/', include(admin.site.urls)),
+	
 ]
