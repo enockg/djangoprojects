c6589e6771a69d079c084a0a48f101d623eafdfc
enockg <enockglidden@hotmail.com>
2015-11-10 16:51:25 -0500

    django projects

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -23,6 +23,6 @@ urlpatterns = patterns('',
 	url(r'^hello/$', hello),
     url(r'^time/$', current_datetime),
     url(r'^time/plus/(\d{1,2})/$', hours_ahead),
-	url(r'^template/$', current_datetime),
+	url(r'^template/$', current_date),
 	
 )
