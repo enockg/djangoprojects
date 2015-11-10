ceba48b90db62c4cd101dd16b84e7ad54afb41a8
enockg <enockglidden@hotmail.com>
2015-11-10 17:47:19 -0500

    django projects

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,5 +1,6 @@
 
-from django.shortcuts import render
+from django.template.loader import get_template
+from django.template import Context
 from django.http import HttpResponse
 import datetime
 
@@ -8,7 +9,9 @@ def hello(request):
 
 def current_datetime(request):
     now = datetime.datetime.now()
-    return render(request, 'current_datetime.html', {'current_date': now})
+    t = get_template('current_datetime.html')
+    html = t.render(Context({'current_date': now}))
+    return HttpResponse(html)
 	
 def hours_ahead(request, offset):
     try:
