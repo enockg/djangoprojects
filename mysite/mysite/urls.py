46e587b37f783ef2055cfb5e84e97ba7fb759e98
enockg <enockglidden@hotmail.com>
2015-11-11 12:09:44 -0500

    django projects

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,16 +13,10 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
-from mysite.views import hello, current_datetime, hours_ahead
-
+from django.conf.urls.defaults import patterns, include, url
+from mysite.views import hello, current_datetime
 
 urlpatterns = patterns('',
-    #url(r'^admin/', include(admin.site.urls)),
-	url(r'^hello/$', hello),
+    url(r'^hello/$', hello),
     url(r'^time/$', current_datetime),
-    url(r'^time/plus/(\d{1,2})/$', hours_ahead),
-	
-	
 )
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -5,11 +5,6 @@ import datetime
 def hello(request):
     return HttpResponse("Hello world")
 
-#def current_datetime(request):
-   # now = datetime.datetime.now()
-#t = get_template('current_datetime.html')
-    #html = t.render(Context({'current_date': now}))
-    #return HttpResponse(html)
 def current_datetime(request):
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % now
