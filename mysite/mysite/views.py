aea0b05312eac1759e2488229db9752c17544576
enockg <enockglidden@hotmail.com>
2015-11-11 13:27:16 -0500

    django projects

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -2,8 +2,8 @@
 from django.http import HttpResponse
 import datetime
 
-def hello(request):
-    return HttpResponse("Hello world")
+#def hello(request):
+    #return HttpResponse("Hello world")
 
 def current_datetime(request):
     now = datetime.datetime.now()
