b5a0b0b7999c083b364b6a3247052f8d1a7a9e85
enockg <enockglidden@hotmail.com>
2015-11-11 08:46:05 -0500

    django projects

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,7 +19,7 @@ from mysite.views import hello, current_datetime, hours_ahead
 
 
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
 	url(r'^hello/$', hello),
     url(r'^time/$', current_datetime),
     url(r'^time/plus/(\d{1,2})/$', hours_ahead),
