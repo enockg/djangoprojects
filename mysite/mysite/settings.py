dcdc260c781f3fe80e35c1e6e9345e02fb17ea62
enockg <enockglidden@hotmail.com>
2015-11-11 15:40:51 -0500

    django projects

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -73,12 +73,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 #https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydb',
-    }
-}
+#DATABASES = {
+   # 'default': {
+       # 'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': 'mydb',
+    #}
+#}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
