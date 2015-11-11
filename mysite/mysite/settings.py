e8d4c0e730ea0923ddbf85f1e8a14314d608b57f
enockg <enockglidden@hotmail.com>
2015-11-11 08:55:30 -0500

    django projects

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['djangoprojects\mysite',],
+        'DIRS': ['/enockg/djangoprojects\mysite',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
